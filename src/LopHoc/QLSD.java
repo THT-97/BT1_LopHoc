606fd836a026fd01d53e15567caaa0c1b9e35223
TranHuyThinh <tranhuythinh97@gmail.com>
2020-04-07 17:27:38 +0700

    Initiation

---
+++ src/LopHoc/QLSD.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LopHoc;
+
+import java.util.ArrayList;
+
+public class QLSD implements IQLDS{
+    ArrayList<CaNhan> ds;
+
+    @Override
+    public int them(CaNhan p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int xoa(CaNhan p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void inDS() {
+        ds.forEach((p) -> p.HienThiTT());
+    }
+}
