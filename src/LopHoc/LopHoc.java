b9de50b54b8ce83801456666fc3761a4a6ef5ab2
TranHuyThinh <tranhuythinh97@gmail.com>
2020-04-08 19:24:06 +0700

    New Class

---
+++ src/LopHoc/LopHoc.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LopHoc;
+
+public class LopHoc {
+    CaNhan giaoVienCN = new GiaoVien("VatLy12", "VatLy", "Ngo Thi Bich", 40, "20 2/4 Nha Trang", "0900123456");
+    QLDS qldsGVGD;
+    QLDS qldsHS;
+    public int themHocSinh(HocSinh hs){throw new UnsupportedOperationException("Not supported yet.");}
+    public int themGiaoVienGD(GiaoVien gv){throw new UnsupportedOperationException("Not supported yet.");}
+    public int inDSHS(){throw new UnsupportedOperationException("Not supported yet.");}
+    public int inDSGVGD(){throw new UnsupportedOperationException("Not supported yet.");}
+}
